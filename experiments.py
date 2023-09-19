c99103174bc4c95aa1e1758112d8ad4ef91d8aa5
coopersigrist <csigrist@umass.edu>
2023-09-19 13:17:08 -0400

    added GFF and GBF

--- experiments.py
+++ experiments.py
@@ -1,6 +1,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 
@@ -47,15 +48,51 @@ def green_worst_fit(arr,g,b):
     
     return cost
 
+def green_best_fit(arr,g,b):
+    cost = 0
+    bins = [0]
+    threshold = min(g + 1/b, 1)
+    for item in arr:
+        allowed = list(filter(lambda n: n < (threshold - item), bins))
+        if len(allowed) == 0:
+            cost += 1
+            bins.append(item)
+        else:
+            bins[bins.index(max(allowed))] += item
+            
+    for bin in bins:
+        cost += max(bin - g, 0) * b
+    
+    return cost
+
+def green_first_fit(arr,g,b):
+    cost = 0
+    bins = [0]
+    threshold = min(g + 1/b, 1)
+    for item in arr:
+        allowed = list(filter(lambda n: n < (threshold - item), bins))
+        if len(allowed) == 0:
+            cost += 1
+            bins.append(item)
+        else:
+            bins[bins.index(allowed[0])] += item
+            
+    for bin in bins:
+        cost += max(bin - g, 0) * b
+    
+    return cost
+
 
 number = 100
 gs = np.round(np.arange(0, 1, 1/number), 4)
-bs = np.flip(np.round(np.arange(0, 1, 1/number), 4))
+bs = np.round(np.arange(0, 2, 2/number), 4)
 
 opt_s_store = np.zeros((number,number))
 nf_s_store = np.zeros((number,number))
 green_next_fit_store = np.zeros((number,number))
 green_worst_fit_store = np.zeros((number,number))
+green_best_fit_store = np.zeros((number,number))
+green_first_fit_store = np.zeros((number,number))
 
 example = np.random.rand(100)
 
@@ -65,69 +102,100 @@ for i,g in enumerate(gs):
         nf_s_store[i,j] = nf_s(example, g, b)
         green_next_fit_store[i,j] = green_next_fit(example, g, b)
         green_worst_fit_store[i,j] = green_worst_fit(example, g, b)
+        green_best_fit_store[i,j] = green_best_fit(example, g, b)
+        green_first_fit_store[i,j] = green_first_fit(example, g, b)
 
-vmin = min(np.min(opt_s_store), np.min(nf_s_store), np.min(green_next_fit_store))
-vmax = max(np.max(opt_s_store), np.max(nf_s_store), np.max(green_next_fit_store))
+# vmin = min(np.min(opt_s_store), np.min(nf_s_store), np.min(green_next_fit_store))
+# vmax = max(np.max(opt_s_store), np.max(nf_s_store), np.max(green_next_fit_store))
 
-plt.subplot(1, 3, 1)
+# plt.subplot(1, 3, 1)
 
-ax = sns.heatmap(opt_s_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax, cbar=False)
-plt.title("values of OPT(S) for varied G and B")
-plt.xlabel("Value of G")
-plt.ylabel("Value of B")
-
-
-plt.subplot(1, 3, 2)
-
-ax = sns.heatmap(nf_s_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax, cbar=False)
-plt.title("values of NF(S) for varied G and B")
-plt.xlabel("Value of G")
-plt.ylabel("Value of B")
+# ax = sns.heatmap(opt_s_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax, cbar=False)
+# plt.title("values of OPT(S) for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
 
 
-plt.subplot(1, 3, 3)
 
-ax = sns.heatmap(green_next_fit_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax)
-plt.title("Cost of Green next fit for varied G and B")
+# plt.subplot(1, 3, 2)
+
+# ax = sns.heatmap(nf_s_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax, cbar=False)
+# plt.title("values of NF(S) for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+
+
+
+# plt.subplot(1, 3, 3)
+
+# ax = sns.heatmap(green_next_fit_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax)
+# plt.title("Cost of Green next fit for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+# plt.show()
+
+# plt.subplot(1, 2, 1)
+
+# ax = sns.heatmap(opt_s_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax, cbar=False)
+# plt.title("values of OPT(S) for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+
+# plt.subplot(1, 2, 2)
+
+# ax = sns.heatmap(green_next_fit_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax)
+# plt.title("Cost of Green worst fit for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+# plt.show()
+
+# ax = sns.heatmap(nf_s_store/opt_s_store, linewidth=0, xticklabels=[], yticklabels=[])
+# plt.title("CR NF(S) / OPT(S) for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+# plt.plot((number/(2 * gs)), number - number*gs, label="G=1/B" )
+# plt.legend()
+# plt.show()
+
+# ax = sns.heatmap(nf_s_store/green_next_fit_store, linewidth=0, xticklabels=[], yticklabels=[])
+# plt.title("Cost of NF(S) / Real Green Next Fit ")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+# plt.plot((number/(2 * gs)), number - number*gs, label="G=1/B" )
+# plt.legend()
+# plt.show()
+
+ax = sns.heatmap(green_best_fit_store/green_worst_fit_store, linewidth=0, xticklabels=[], yticklabels=[])
+plt.title("Cost of GBF / GWF ")
 plt.xlabel("Value of G")
 plt.ylabel("Value of B")
 plt.show()
 
-plt.subplot(1, 2, 1)
-
-ax = sns.heatmap(opt_s_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax, cbar=False)
-plt.title("values of OPT(S) for varied G and B")
-plt.xlabel("Value of G")
-plt.ylabel("Value of B")
-
-plt.subplot(1, 2, 2)
-
-ax = sns.heatmap(green_next_fit_store, linewidth=0, xticklabels=[], yticklabels=[], vmin=vmin, vmax=vmax)
-plt.title("Cost of Green worst fit for varied G and B")
+ax = sns.heatmap(green_first_fit_store/green_worst_fit_store, linewidth=0, xticklabels=[], yticklabels=[])
+plt.title("Cost of GFF / GWF ")
 plt.xlabel("Value of G")
 plt.ylabel("Value of B")
 plt.show()
 
-ax = sns.heatmap(nf_s_store/opt_s_store, linewidth=0, xticklabels=[], yticklabels=[])
-plt.title("CR NF(S) / OPT(S) for varied G and B")
-plt.xlabel("Value of G")
-plt.ylabel("Value of B")
-plt.show()
+# ax = plt.subplot(1, 2, 1)
 
-plt.subplot(1, 2, 1)
+# sns.heatmap(green_next_fit_store/opt_s_store, linewidth=0, xticklabels=[], yticklabels=[], ax=ax)
+# plt.title("CR Green next fit / OPT(S) for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+# plt.plot((number/(2 * gs)), number - number*gs, label="G=1/B" )
+# plt.legend()
 
-ax = sns.heatmap(green_next_fit_store/opt_s_store, linewidth=0, xticklabels=[], yticklabels=[])
-plt.title("CR Green next fit / OPT(S) for varied G and B")
-plt.xlabel("Value of G")
-plt.ylabel("Value of B")
 
-plt.subplot(1, 2, 2)
 
-ax = sns.heatmap(green_worst_fit_store/opt_s_store, linewidth=0, xticklabels=[], yticklabels=[])
-plt.title("CR Green worst fit / OPT(S) for varied G and B")
-plt.xlabel("Value of G")
-plt.ylabel("Value of B")
-plt.show()
+# plt.subplot(1, 2, 2)
+
+# ax = sns.heatmap(green_worst_fit_store/opt_s_store, linewidth=0, xticklabels=[], yticklabels=[])
+# plt.title("CR Green worst fit / OPT(S) for varied G and B")
+# plt.xlabel("Value of G")
+# plt.ylabel("Value of B")
+# plt.plot((number/(2 * gs)), number - number*gs, label="G=1/B" )
+# plt.show()
 
 # plt.imshow(opt_s_store, cmap='hot', interpolation='nearest')
 
